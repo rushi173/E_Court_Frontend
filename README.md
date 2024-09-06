ebeab6b0618f282b7a81a61fb8b6a322c1d711b3
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-09-06 16:13:49 +0530

    made in flow

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# EJustice Bharat Frontend
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import LoginForm from './Components/LoginForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
